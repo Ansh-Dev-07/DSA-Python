340be359f3573a36b4c02e4211db7adef800824a
Ansh-Dev-07 <anshuu.exe@gmail.com>
2026-04-02 20:36:46 +0530

    added more codes

---
+++ Day6.py
@@ -0,0 +1,56 @@
+# count vowel in string
+# id="d6q1"
+# s="anshsoni"
+# vov_count=0
+# if(s==""):
+#     print("empty string")
+# for i in s:
+#     if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u"):
+#         vov_count+=1
+# print(vov_count)
+
+# reverse a string without slicing
+# id="d6q2"
+# s="hello"
+# rev=""
+# if(s==""):
+#     print("empty string")
+# for i in range(len(s)-1,-1,-1):
+#     rev+=s[i]
+# print(rev)
+
+# check if string is palindrome
+# id="d6q3"
+# s="madam"
+# s="hello"
+# rev=""
+# if(s==""):
+#     print("empty string")
+# for i in range(len(s)-1,-1,-1):
+#     rev+=s[i]
+# if(s==rev):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# count digits in string
+# id="d6q4"
+# s="a1b2c3"
+# dig_count=0
+# if(s==""):
+#     print("empty string")
+# for i in s:
+#     if(i=="0" or i=="1" or i=="2" or i=="3" or i=="4" or i=="5" or i=="6" or i=="7" or i=="8" or i=="9"):
+#         dig_count+=1
+# print(dig_count)
+
+# find largest number in array
+# id="d6q5"
+# arr=[10,5,20,8]
+# large_val=float('-inf')
+# if(arr==[]):
+#     print("empty list")
+# for i in range(len(arr)):
+#     if(arr[i] > large_val):
+#         large_val=arr[i]
+# print(large_val)
